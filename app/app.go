42a52d82aff1ef83e2a3cee6ab451eeda99e07c7
Ahsan <mohd.ahsan.mirza@gmail.com>
2021-04-10 16:07:03 -0400

    Adding sql connection

--- app/app.go
+++ app/app.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"bufio"
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
+
+	dbConnectionString := "postgres://" + os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + "/" + os.Getenv("DB_NAME")
+	db, err := sql.Open("postgres", dbConnectionString)
+	if err != nil {
+		log.Fatal("Failed to open a DB connection: ", err)
+	}
+	defer db.Close()
 
 	file, fileOpenErr := os.Open(os.Getenv("INPUT_FILE"))
 	if fileOpenErr != nil {
