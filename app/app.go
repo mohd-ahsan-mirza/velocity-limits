05ada14be5019f06c3ba62f68c3d2c1e0047f691
Ahsan <mohd.ahsan.mirza@gmail.com>
2021-04-10 19:53:54 -0400

    Fixing db connection issus

--- app/app.go
+++ app/app.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"database/sql"
-	"fmt"
 	"log"
 	"os"
 
@@ -12,12 +12,35 @@ import (
 
 func main() {
 
-	dbConnectionString := "postgres://" + os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + "/" + os.Getenv("DB_NAME")
-	db, err := sql.Open("postgres", dbConnectionString)
-	if err != nil {
-		log.Fatal("Failed to open a DB connection: ", err)
+	dbConnectionString := "postgres://" + os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + "/" + os.Getenv("DB_NAME") + "?sslmode=disable"
+
+	db, dbOpenErr := sql.Open("postgres", dbConnectionString)
+	if dbOpenErr != nil {
+		log.Fatal("Failed to open a DB connection: ", dbOpenErr)
 	}
 	defer db.Close()
+
+	ctx := context.Background()
+
+	tx, txErr := db.BeginTx(ctx, nil)
+	if txErr != nil {
+		log.Fatal(txErr)
+	}
+	rows, qCtxErr := tx.QueryContext(ctx, "SELECT NOW();")
+	if qCtxErr != nil {
+		log.Fatal(qCtxErr)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var name string
+		if err := rows.Scan(&name); err != nil {
+			// Check for a scan error.
+			// Query rows will be closed with defer.
+			log.Fatal(err)
+		}
+		println(name)
+	}
 
 	file, fileOpenErr := os.Open(os.Getenv("INPUT_FILE"))
 	if fileOpenErr != nil {
@@ -27,9 +50,8 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		//fmt.Println(scanner.Text())
 	}
-
 	if scannerError := scanner.Err(); scannerError != nil {
 		log.Fatal(scannerError)
 	}
