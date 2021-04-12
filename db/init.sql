472d678a6a2353f410533e67a6e0ea36f1ba79e8
Ahsan <mohd.ahsan.mirza@gmail.com>
2021-04-12 00:27:55 -0400

    Adding query

--- db/init.sql
+++ db/init.sql
@@ -9,4 +9,21 @@ created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 updated TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 SET TIME ZONE 'UTC';
--- select date_trunc('week', current_date) as current_week
+/*
+SELECT
+	*
+FROM
+	load_transaction_history
+WHERE
+	customer_id = 834
+	AND date_trunc('week', transaction_time) = (
+		SELECT
+			date_trunc('week', (
+					SELECT
+						transaction_time FROM load_transaction_history
+					WHERE
+						customer_id = 834
+					ORDER BY
+						customer_id, transaction_time DESC
+					LIMIT 1)));
+*/
