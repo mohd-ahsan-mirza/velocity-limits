ff49de8c03f293ef7ea66f8a6a36ad267c816489
Ahsan <mohd.ahsan.mirza@gmail.com>
2021-04-11 20:12:34 -0400

    Changing some stuff

--- app/internal/db/db.go
+++ app/internal/db/db.go
@@ -1,7 +1,19 @@
 package db
 
-import "database/sql"
+import (
+	"app/internal"
+	"database/sql"
+)
 
-type db struct {
+type dbsql struct {
 	db *sql.DB
+}
+
+//New method
+func New(db *sql.DB) internal.Db {
+	return &dbsql{db: db}
+}
+
+func (s *dbsql) InsertLoadTransactionRecord(*internal.LoadTransactionRecord) (bool, error) {
+	return true, nil
 }
