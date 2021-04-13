4cb7325d3717b4c54d49cc26132c7957a4caf6b7
Ahsan <mohd.ahsan.mirza@gmail.com>
2021-04-12 22:53:24 -0400

    Adding some changes

--- app/internal/service/service.go
+++ app/internal/service/service.go
@@ -4,10 +4,10 @@ import (
 	"app/internal"
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
 	"sort"
 	"strings"
-	"time"
 
 	dbsql "app/internal/dbsql"
 )
@@ -32,16 +32,27 @@ func (s *service) LoadFunds(request string) (bool, []byte, error) {
 		return true, nil, nil
 	}
 
+	unacceptedTransactionRespStr := `{"id":"` + loadTransactionRecord.ID +
+		`","customer_id":"` + loadTransactionRecord.CustomerID + `","accepted": false}`
+
 	// Getting all transactions that happened within the week of the last transaction
 	records := s.db.GetAllRecordsForLatestTransactionByCustomerID("week", loadTransactionRecord.CustomerID)
 	// sorting the records by latest transaction time so the last transaction date will be the first record in the array
 	sort.Slice(records, func(i, j int) bool {
 		return records[i].TransactionTime.After(records[j].TransactionTime)
 	})
-	var lastTransactionTimeStamp time.Time
+
 	// If the customer is loading funds the first time none of the following checks are required
 	if len(records) > 0 {
-		lastTransactionTimeStamp = records[0].TransactionTime
+		latestTransactionTimeStamp := loadTransactionRecord.TransactionTime
+		var allTransactionRecordsOfLastTransactionDate []internal.LoadTransactionRecord
+
+		for _, record := range records {
+			if DateEqual(latestTransactionTimeStamp, record.TransactionTime) {
+				allTransactionRecordsOfLastTransactionDate = append(allTransactionRecordsOfLastTransactionDate, record)
+				fmt.Println(allTransactionRecordsOfLastTransactionDate)
+			}
+		}
 
 		// A maximum of $20,000 can be loaded per week
 		// A maximum of 3 loads can be performed per day, regardless of amount
@@ -59,8 +70,7 @@ func (s *service) LoadFunds(request string) (bool, []byte, error) {
 		responseStr = `{"id":"` + loadTransactionRecord.ID +
 			`","customer_id":"` + loadTransactionRecord.CustomerID + `","accepted": true}`
 	} else {
-		responseStr = `{"id":"` + loadTransactionRecord.ID +
-			`","customer_id":"` + loadTransactionRecord.CustomerID + `","accepted": false}`
+		responseStr = unacceptedTransactionRespStr
 	}
 	response, _ := json.Marshal(responseStr)
 	return false, response, nil
